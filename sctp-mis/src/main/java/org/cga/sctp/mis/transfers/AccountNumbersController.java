faf6e4daefd3c4af7afb5c3bf55b398270933375
Zikani Nyirenda Mwase <zikaninyirenda@cgatechnologies.org.uk>
2022-04-26 15:28:36 +0200

    fix: Modify upload helper function to return path of file

--- sctp-mis/src/main/java/org/cga/sctp/mis/transfers/AccountNumbersController.java
+++ sctp-mis/src/main/java/org/cga/sctp/mis/transfers/AccountNumbersController.java
@@ -36,8 +36,13 @@ import me.desair.tus.server.TusFileUploadService;
 import me.desair.tus.server.exception.TusException;
 import me.desair.tus.server.upload.UploadInfo;
 import org.cga.sctp.mis.core.SecuredBaseController;
+import org.cga.sctp.mis.file_upload.FileUploadService;
 import org.cga.sctp.targeting.exchange.DataImport;
 import org.cga.sctp.targeting.exchange.DataImportObject;
+import org.cga.sctp.transfers.TransferService;
+import org.cga.sctp.transfers.TransferSession;
+import org.cga.sctp.transfers.epayments.TransferAccountNumberList;
+import org.cga.sctp.transfers.periods.TransferPeriod;
 import org.cga.sctp.user.AdminAndStandardAccessOnly;
 import org.cga.sctp.user.AuthenticatedUser;
 import org.cga.sctp.user.AuthenticatedUserDetails;
@@ -50,13 +55,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/transfers/assign-accounts")
-public class AccountNumbersController extends SecuredBaseController  {
+public class AccountNumbersController extends SecuredBaseController {
+
+    @Autowired
+    private FileUploadService fileUploadService;
 
     @Autowired
     private TusFileUploadService tusFileUploadService;
+
+    @Autowired
+    private TransferService transferService;
 
     @GetMapping
     @AdminAndStandardAccessOnly
@@ -68,49 +81,34 @@ public class AccountNumbersController extends SecuredBaseController  {
                 .addObject("accounts", new Object());
     }
 
+    @PostMapping
     @RequestMapping(
             value = {"/{session-id}/upload", "/{session-id}/upload/**"},
             method = {RequestMethod.POST, RequestMethod.PATCH,
                     RequestMethod.HEAD, RequestMethod.DELETE,
                     RequestMethod.OPTIONS, RequestMethod.GET}
     )
-    public void processUpload(
-            @PathVariable("session-id") Long sessionId,
-            @AuthenticatedUserDetails AuthenticatedUser user,
-            HttpServletRequest servletRequest,
-            HttpServletResponse servletResponse) throws IOException {
-/*
-        UploadInfo uploadInfo;
-        DataImport dataImport = importService.findImportByIdAndStatus(sessionId,
-                DataImportObject.ImportSource.UBR_CSV,
-                DataImportObject.ImportStatus.FileUploadPending);
+    public void processUpload(@PathVariable("session-id") Long sessionId,
+                              @AuthenticatedUserDetails AuthenticatedUser user,
+                              HttpServletRequest servletRequest,
+                              HttpServletResponse servletResponse) throws IOException {
 
-        if (dataImport == null || !dataImport.getImporterUserId().equals(user.id())) {
-            servletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            return;
-        }
-
-        // process file
         tusFileUploadService.process(servletRequest, servletResponse, user.username());
 
         try {
-            uploadInfo = tusFileUploadService.getUploadInfo(servletRequest.getRequestURI(), user.username());
+            UploadInfo uploadInfo = tusFileUploadService.getUploadInfo(servletRequest.getRequestURI(), user.username());
             if (uploadInfo != null) {
                 if (!uploadInfo.isUploadInProgress()) {
                     try (InputStream is = tusFileUploadService.getUploadedBytes(servletRequest.getRequestURI(), user.username())) {
-                        if (fileUploadService.moveToStagingDirectory(is, uploadInfo)) {
-                            // Update status to processing
-                            dataImport.setStatus(DataImportObject.ImportStatus.Processing);
-                            dataImport.setStatusText("Queued for import by task");
-                            dataImport.setSourceFile(uploadInfo.getId().toString());
-                            importService.saveDataImport(dataImport);
-                            fileImportService.queueImport(dataImport);
+                        Optional<Path> uploadedFilePath = fileUploadService.moveToStagingDirectoryAndGetPath(is, uploadInfo);
+                        if (uploadedFilePath.isEmpty()) {
+                            TransferSession transferSession = null;
+                            TransferPeriod transferPeriod = null;
+                            TransferAccountNumberList accountNumberList = extractAccountNumberFromCSV(uploadedFilePath.get());
+                            transferService.assignAccountNumbers(transferSession, transferPeriod, accountNumberList);
                         }
                     }
-
-                    // upload is finished
-                    // move to staging directory
-                    // Update to processing
+                    // upload is finished, move to staging directory, Update to processing
                     // delete from temp
                     tusFileUploadService.deleteUpload(servletRequest.getRequestURI(), user.username());
                 }
@@ -119,7 +117,16 @@ public class AccountNumbersController extends SecuredBaseController  {
             LOG.error("Error getting upload information", e);
             servletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
-        */
     }
 
+    /**
+     * Extract data from CSV
+     * @param uploadedFilePath path to uploaded file
+     * @return transfer account number list
+     */
+    public TransferAccountNumberList extractAccountNumberFromCSV(Path uploadedFilePath) {
+        TransferAccountNumberList transferAccountNumberList = new TransferAccountNumberList();
+        //  TODO: extract account numbers from the CSV file
+        return transferAccountNumberList;
+    }
 }
